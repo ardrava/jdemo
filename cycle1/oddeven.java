34fb1ef127f8ab354bc8cc1a607ca9018d475bd4
ardrava <98315389+ardrava@users.noreply.github.com>
2022-01-25 16:20:48 +0530

    Create oddeven.java

---
+++ cycle1/oddeven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class oddeven
+{
+  public static void main (String args [])
+  {
+   Scanner sc=new Scanner (System.in);
+   System.out.println("Enter the number");
+   int n=sc.nextInt();
+
+   if(n%2==0)
+    {
+      System.out.println("Number is even");
+    }
+   else
+    {
+      System.out.println("Number is odd");
+    }
+   }
+}
