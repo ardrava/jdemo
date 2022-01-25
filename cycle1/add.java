02d96a206065b396c3a57765cc2809fd91a34d3f
ardrava <98315389+ardrava@users.noreply.github.com>
2022-01-25 16:11:59 +0530

    Create add.java

---
+++ cycle1/add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class add
+{
+  public static void main (String args [])
+  {
+    int a,b,sum=0;
+    Scanner sc= new Scanner (System.in);
+    System.out.println("Enter the numbers to find the sum");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    sum=a+b;
+    System.out.println("The sum is:"+sum);
+  }
+}
