395acc4bfae0b361416b08249c4fbc33d6b807a7
ardrava <98315389+ardrava@users.noreply.github.com>
2022-02-26 16:53:41 +0530

    Create arithmetic.java

---
+++ cycle 6/arithmetic.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class arithmetic {
+public static void main(String[] args) {
+		int firstNum = 25;
+		int secondNum = 20%2; 
+		
+		int div = firstNum/secondNum;   
+		System.out.println("Result : "+ div);
+                System.out.println("After division");
+	}
+}
