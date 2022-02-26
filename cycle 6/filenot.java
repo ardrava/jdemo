8b6fa0ae21a89280787fbbd79175430cb18e730c
ardrava <98315389+ardrava@users.noreply.github.com>
2022-02-26 17:03:24 +0530

    Create filenot.java

---
+++ cycle 6/filenot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class filenot
+{
+   public static void main(String[] args) 
+  {
+    FileReader reader = new FileReader("file.txt");
+    BufferedReader br = new BufferedReader(reader);
+    String data =null;
+     
+    while ((data = br.readLine()) != null) 
+    {
+        System.out.println(data);
+    }
+    br.close();
+  }
+}
